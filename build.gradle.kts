16a8f1238f9f7830d8100165b6da2a2822785632
attiasas <assafattias93@gmail.com>
2023-06-21 10:23:59 +0300

    Improvements after Review on Java Impl removed repo resolution as feature from readme changed Java compatible ver to 11 clean up on build script vul and file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 group = "org.jfrog.buildinfo"
 
-val pluginDescription = "JFrog Gradle plugin for Build Info extraction and Artifactory publishing."
+val pluginDescription = "JFrog Gradle plugin publishes artifacts to Artifactory and handles the collection and publishing of Build Info."
 val functionalTest by sourceSets.creating
 
 plugins {
@@ -20,27 +20,27 @@ val commonsTxtVersion = "1.10.0"
 val testNgVersion = "7.7.1"
 
 dependencies {
-    implementation ("org.jfrog.buildinfo","build-info-extractor",buildInfoVersion)
-    implementation ("org.jfrog.buildinfo","build-info-api",buildInfoVersion)
-    implementation ("org.jfrog.buildinfo","build-info-client",buildInfoVersion)
-    implementation ("org.jfrog.filespecs","file-specs-java",fileSpecsVersion)
+    implementation("org.jfrog.buildinfo","build-info-extractor", buildInfoVersion)
+    implementation("org.jfrog.buildinfo","build-info-api", buildInfoVersion)
+    implementation("org.jfrog.buildinfo","build-info-client", buildInfoVersion)
+    implementation("org.jfrog.filespecs","file-specs-java", fileSpecsVersion)
 
-    implementation ("org.apache.commons", "commons-lang3",commonsLangVersion)
-    implementation ("org.apache.ivy", "ivy","2.5.1")
-    implementation ("com.google.guava", "guava","31.1-jre")
+    implementation("org.apache.commons", "commons-lang3", commonsLangVersion)
+    implementation("org.apache.ivy", "ivy","2.5.1")
+    implementation("com.google.guava", "guava","32.0.1-jre")
 
-    testImplementation ("org.testng","testng",testNgVersion)
+    testImplementation("org.testng","testng", testNgVersion)
 
-    "functionalTestImplementation" ("org.jfrog.buildinfo","build-info-extractor",buildInfoVersion)
-    "functionalTestImplementation" ("org.jfrog.buildinfo","build-info-api",buildInfoVersion)
-    "functionalTestImplementation" ("org.jfrog.filespecs","file-specs-java",fileSpecsVersion)
+    "functionalTestImplementation"("org.jfrog.buildinfo","build-info-extractor", buildInfoVersion)
+    "functionalTestImplementation"("org.jfrog.buildinfo","build-info-api", buildInfoVersion)
+    "functionalTestImplementation"("org.jfrog.filespecs","file-specs-java", fileSpecsVersion)
 
-    "functionalTestImplementation" ("org.testng","testng",testNgVersion)
-    "functionalTestImplementation" ("org.apache.commons", "commons-lang3",commonsLangVersion)
-    "functionalTestImplementation" ("org.apache.commons", "commons-text",commonsTxtVersion)
-    "functionalTestImplementation" ("commons-io", "commons-io",commonsIoVersion)
-    "functionalTestImplementation" ("org.apache.httpcomponents", "httpclient","4.5.14")
-    "functionalTestImplementation" (project(mapOf("path" to ":")))
+    "functionalTestImplementation"("org.testng","testng", testNgVersion)
+    "functionalTestImplementation"("org.apache.commons", "commons-lang3", commonsLangVersion)
+    "functionalTestImplementation"("org.apache.commons", "commons-text", commonsTxtVersion)
+    "functionalTestImplementation"("commons-io", "commons-io", commonsIoVersion)
+    "functionalTestImplementation"("org.apache.httpcomponents", "httpclient","4.5.14")
+    "functionalTestImplementation"(project(mapOf("path" to ":")))
 
 }
 
@@ -58,8 +58,8 @@ gradlePlugin {
 }
 
 tasks.compileJava {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
 }
 
 publishing {
@@ -114,8 +114,6 @@ tasks.withType<Test>().configureEach {
         events("started", "passed", "skipped", "failed", "standardOut", "standardError")
         minGranularity = 0
     }
-//    dependsOn("pluginUnderTestMetadata")
-//    dependsOn("assemble")
 }
 
 val functionalTestTask = tasks.register<Test>("functionalTest") {
@@ -124,10 +122,6 @@ val functionalTestTask = tasks.register<Test>("functionalTest") {
     testClassesDirs = functionalTest.output.classesDirs
     classpath = functionalTest.runtimeClasspath
     mustRunAfter(tasks.test)
-//    resources = file("f")
-//    resources {
-//        srcDir file('build/pluginUnderTestMetadata')
-//    }
 }
 
 tasks.check {
