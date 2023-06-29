18f66caf41c1a34e612a942689b3565a390d067f
attiasas <assafattias93@gmail.com>
2023-06-29 11:50:08 +0300

    Fix plugin publishing configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ val functionalTest by sourceSets.creating
 plugins {
     `java-gradle-plugin`
     `maven-publish`
+    signing
 }
 
 repositories {
@@ -20,27 +21,27 @@ val commonsTxtVersion = "1.10.0"
 val testNgVersion = "7.7.1"
 
 dependencies {
-    implementation("org.jfrog.buildinfo","build-info-extractor", buildInfoVersion)
-    implementation("org.jfrog.buildinfo","build-info-api", buildInfoVersion)
-    implementation("org.jfrog.buildinfo","build-info-client", buildInfoVersion)
-    implementation("org.jfrog.filespecs","file-specs-java", fileSpecsVersion)
+    implementation("org.jfrog.buildinfo", "build-info-extractor", buildInfoVersion)
+    implementation("org.jfrog.buildinfo", "build-info-api", buildInfoVersion)
+    implementation("org.jfrog.buildinfo", "build-info-client", buildInfoVersion)
+    implementation("org.jfrog.filespecs", "file-specs-java", fileSpecsVersion)
 
     implementation("org.apache.commons", "commons-lang3", commonsLangVersion)
-    implementation("org.apache.ivy", "ivy","2.5.1")
-    implementation("com.google.guava", "guava","32.0.1-jre")
+    implementation("org.apache.ivy", "ivy", "2.5.1")
+    implementation("com.google.guava", "guava", "32.0.1-jre")
 
-    testImplementation("org.testng","testng", testNgVersion)
-    testImplementation ("org.mockito", "mockito-core" ,"3.+")
+    testImplementation("org.testng", "testng", testNgVersion)
+    testImplementation("org.mockito", "mockito-core", "3.+")
 
-    "functionalTestImplementation"("org.jfrog.buildinfo","build-info-extractor", buildInfoVersion)
-    "functionalTestImplementation"("org.jfrog.buildinfo","build-info-api", buildInfoVersion)
-    "functionalTestImplementation"("org.jfrog.filespecs","file-specs-java", fileSpecsVersion)
+    "functionalTestImplementation"("org.jfrog.buildinfo", "build-info-extractor", buildInfoVersion)
+    "functionalTestImplementation"("org.jfrog.buildinfo", "build-info-api", buildInfoVersion)
+    "functionalTestImplementation"("org.jfrog.filespecs", "file-specs-java", fileSpecsVersion)
 
-    "functionalTestImplementation"("org.testng","testng", testNgVersion)
+    "functionalTestImplementation"("org.testng", "testng", testNgVersion)
     "functionalTestImplementation"("org.apache.commons", "commons-lang3", commonsLangVersion)
     "functionalTestImplementation"("org.apache.commons", "commons-text", commonsTxtVersion)
     "functionalTestImplementation"("commons-io", "commons-io", commonsIoVersion)
-    "functionalTestImplementation"("org.apache.httpcomponents", "httpclient","4.5.14")
+    "functionalTestImplementation"("org.apache.httpcomponents", "httpclient", "4.5.14")
     "functionalTestImplementation"(project(mapOf("path" to ":")))
 
 }
@@ -63,38 +64,18 @@ tasks.compileJava {
     targetCompatibility = "11"
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            pom {
-                name.set(rootProject.name)
-                description.set(pluginDescription)
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("JFrog")
-                        email.set("eco-system@jfrog.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/jfrog/artifactory-gradle-plugin.git")
-                    developerConnection.set("scm:git:git@github.com/jfrog/artifactory-gradle-plugin.git")
-                    url.set("https://github.com/jfrog/artifactory-gradle-plugin")
-                }
-            }
-            from(components["java"])
-        }
-    }
+// Build configurations
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allJava)
+    archiveClassifier.set("sources")
 }
 
-// Build configurations
-tasks.register<Jar>("uberJar") {
+val javadocJar by tasks.registering(Jar::class) {
+    from(tasks.named("javadoc"))
+    archiveClassifier.set("javadoc")
+}
+
+val uberJar by tasks.register<Jar>("uberJar") {
     archiveClassifier.set("uber")
     // Include the project classes
     from(sourceSets.main.get().output)
@@ -109,6 +90,48 @@ tasks.register<Jar>("uberJar") {
 tasks.named<Jar>("jar") {
     dependsOn("uberJar")
     exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+}
+
+publishing {
+    val publication = publications.create<MavenPublication>("mavenJava") {
+        artifactId = project.name
+
+        artifact(javadocJar)
+        artifact(sourcesJar)
+        artifact(uberJar)
+
+        pom {
+            name.set(rootProject.name)
+            description.set(pluginDescription)
+
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    name.set("JFrog")
+                    email.set("eco-system@jfrog.com")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/jfrog/artifactory-gradle-plugin.git")
+                developerConnection.set("scm:git:git@github.com/jfrog/artifactory-gradle-plugin.git")
+                url.set("https://github.com/jfrog/artifactory-gradle-plugin")
+            }
+        }
+        from(components["java"])
+    }
+
+    extensions.configure(SigningExtension::class.java) {
+        isRequired = project.hasProperty("sign")
+        val signingKey = findProperty("signingKey") as String?
+        val signingPassword = findProperty("signingPassword") as String?
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publication)
+    }
 }
 
 // Tests configurations
