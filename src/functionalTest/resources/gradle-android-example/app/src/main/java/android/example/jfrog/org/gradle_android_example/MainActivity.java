e2cf726b9aafe4e33b9d228f4adf300303532272
Yahav Itzhak <yahavi@users.noreply.github.com>
2023-08-13 12:33:13 +0300

    Fail to resolve dependencies on a multi-module Android project (#46)

---
+++ src/functionalTest/resources/gradle-android-example/app/src/main/java/android/example/jfrog/org/gradle_android_example/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.jfrog.org.gradle_android_example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
