1aec4df01002febd2e4c607ad15cdcb1a4c6287d
yahavi <yahavi@jfrog.com>
2023-11-10 17:04:34 +0200

    Support Gradle 8.4 and above

---
+++ src/main/java/org/jfrog/gradle/plugin/artifactory/extractor/publication/MavenPublicationExtractor.java
@@ -0,0 +1,118 @@
+package org.jfrog.gradle.plugin.artifactory.extractor.publication;
+
+import org.apache.ivy.core.IvyPatternHelper;
+import org.gradle.api.publish.Publication;
+import org.gradle.api.publish.maven.MavenArtifact;
+import org.gradle.api.publish.maven.MavenPublication;
+import org.gradle.api.publish.maven.tasks.GenerateMavenPom;
+import org.jfrog.build.extractor.clientConfiguration.ArtifactoryClientConfiguration;
+import org.jfrog.build.extractor.clientConfiguration.LayoutPatterns;
+import org.jfrog.build.extractor.clientConfiguration.deploy.DeployDetails;
+import org.jfrog.gradle.plugin.artifactory.extractor.PublishArtifactInfo;
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask;
+import org.jfrog.gradle.plugin.artifactory.utils.ExtensionsUtils;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.jfrog.gradle.plugin.artifactory.utils.PublicationUtils.addArtifactInfoToDeployDetails;
+import static org.jfrog.gradle.plugin.artifactory.utils.PublicationUtils.createArtifactBuilder;
+
+public class MavenPublicationExtractor extends PublicationExtractor<MavenPublication> {
+
+    public MavenPublicationExtractor(ArtifactoryTask artifactoryTask) {
+        super(artifactoryTask);
+    }
+
+    @Override
+    public void extractDeployDetails(MavenPublication publication) {
+        // First adding the Maven descriptor (if the build is configured to add it):
+        extractMavenDescriptor(publication);
+
+        // Then extract artifacts
+        extractMavenArtifacts(publication);
+    }
+
+    @Override
+    protected void addArtifactToDeployDetails(MavenPublication publication, DeployDetails.Builder builder, PublishArtifactInfo artifactInfo) {
+        Map<String, String> extraTokens = artifactInfo.getExtraTokens();
+        String artifactPath = IvyPatternHelper.substitute(
+                LayoutPatterns.M2_PATTERN, publication.getGroupId().replace(".", "/"),
+                publication.getArtifactId(),
+                publication.getVersion(),
+                artifactInfo.getName(), artifactInfo.getType(),
+                artifactInfo.getExtension(), publication.getName(),
+                extraTokens, null);
+        builder.artifactPath(artifactPath);
+        addArtifactInfoToDeployDetails(artifactoryTask, publication.getName(), builder, artifactInfo, artifactPath);
+    }
+
+    @Override
+    protected String getPublicationArtifactId(MavenPublication publication) {
+        return publication.getArtifactId();
+    }
+
+    @Override
+    protected boolean isApplicablePublication(Publication publication) {
+        return publication instanceof MavenPublication;
+    }
+
+    /**
+     * Extract deploy details of the Maven descriptor, if configured to add it and stores them at the given task destination
+     */
+    private void extractMavenDescriptor(MavenPublication publication) {
+        if (!isPublishMaven()) {
+            return;
+        }
+        GenerateMavenPom generateMavenPom = artifactoryTask.getProject().getTasks().withType(GenerateMavenPom.class).stream().findFirst().orElse(null);
+        if (generateMavenPom == null) {
+            return;
+        }
+        File pomFile = generateMavenPom.getDestination();
+        if (!pomFile.exists()) {
+            return;
+        }
+        DeployDetails.Builder builder = createArtifactBuilder(pomFile, publication.getName());
+        PublishArtifactInfo artifactInfo = new PublishArtifactInfo(
+                publication.getArtifactId(), "pom", "pom", null, pomFile);
+        addArtifactToDeployDetails(publication, builder, artifactInfo);
+    }
+
+    /**
+     * Checks if the given task should publish the Maven descriptor
+     * Checks global publisher config if exists, if not exists checks specific task configuration
+     */
+    private boolean isPublishMaven() {
+        ArtifactoryClientConfiguration.PublisherHandler publisher = ExtensionsUtils.getPublisherHandler(artifactoryTask.getProject());
+        if (publisher == null) {
+            return false;
+        }
+        // Get the value from the client publisher configuration (in case a CI plugin configuration is used):
+        Boolean publishPom = publisher.isMaven();
+        // It the value is null, it means that there's no CI plugin configuration, so the value should be taken from the
+        // artifactory DSL inside the gradle script:
+        if (publishPom == null) {
+            publishPom = artifactoryTask.getPublishPom();
+        }
+        return publishPom != null ? publishPom : true;
+    }
+
+    /**
+     * Extract deploy details of the Maven artifacts and stores them at the given task destination
+     */
+    private void extractMavenArtifacts(MavenPublication publication) {
+        for (MavenArtifact artifact : publication.getArtifacts()) {
+            createPublishArtifactInfoAndAddToDeployDetails(artifact, publication);
+        }
+    }
+
+    private void createPublishArtifactInfoAndAddToDeployDetails(MavenArtifact artifact, MavenPublication publication) {
+        File file = artifact.getFile();
+        DeployDetails.Builder builder = createArtifactBuilder(file, publication.getName());
+        PublishArtifactInfo artifactInfo = new PublishArtifactInfo(
+                publication.getArtifactId(), artifact.getExtension(),
+                artifact.getExtension(), artifact.getClassifier(),
+                file);
+        addArtifactToDeployDetails(publication, builder, artifactInfo);
+    }
+}
---
+++ src/main/java/org/jfrog/gradle/plugin/artifactory/extractor/publication/PublicationExtractor.java
@@ -0,0 +1,76 @@
+package org.jfrog.gradle.plugin.artifactory.extractor.publication;
+
+import org.gradle.api.publish.Publication;
+import org.gradle.api.publish.tasks.GenerateModuleMetadata;
+import org.jfrog.build.extractor.clientConfiguration.deploy.DeployDetails;
+import org.jfrog.gradle.plugin.artifactory.extractor.PublishArtifactInfo;
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask;
+
+import java.io.File;
+
+import static org.jfrog.gradle.plugin.artifactory.utils.PublicationUtils.createArtifactBuilder;
+
+public abstract class PublicationExtractor<ActualPublication extends Publication> {
+    protected ArtifactoryTask artifactoryTask;
+
+    public PublicationExtractor(ArtifactoryTask artifactoryTask) {
+        this.artifactoryTask = artifactoryTask;
+    }
+
+    /**
+     * Extract publication artifacts, creates deploy details for it and stores them at the given destination
+     *
+     * @param publication - publication to extract details from
+     */
+    public abstract void extractDeployDetails(ActualPublication publication);
+
+    /**
+     * Adds a given artifact to deploy details in the given task destination.
+     *
+     * @param publication  - Maven or Ivy publication
+     * @param builder      - Deploy details builder
+     * @param artifactInfo - The artifact info
+     */
+    protected abstract void addArtifactToDeployDetails(ActualPublication publication, DeployDetails.Builder builder,
+                                                       PublishArtifactInfo artifactInfo);
+
+    /**
+     * Return the artifact ID of the publication.
+     *
+     * @param publication - Maven or Ivy publication
+     * @return the artifact ID of the publication
+     */
+    protected abstract String getPublicationArtifactId(ActualPublication publication);
+
+    /**
+     * Return true if the input publication if of the type of subclass publication type.
+     *
+     * @param publication - The publication to check
+     * @return true if the input publication if of the type of subclass publication type
+     */
+    protected abstract boolean isApplicablePublication(Publication publication);
+
+    /**
+     * Extract *.module files publications.
+     */
+    public void extractModuleInfo() {
+        for (GenerateModuleMetadata generateModuleMetadata : artifactoryTask.getProject().getTasks().withType(GenerateModuleMetadata.class)) {
+            Publication publication = generateModuleMetadata.getPublication().get();
+            if (!isApplicablePublication(publication)) {
+                return;
+            }
+
+            File moduleMetadata = generateModuleMetadata.getOutputFile().getAsFile().get();
+            if (!moduleMetadata.exists()) {
+                return;
+            }
+
+            DeployDetails.Builder builder = createArtifactBuilder(moduleMetadata, generateModuleMetadata.getPublication().get().getName());
+
+            //noinspection unchecked
+            ActualPublication actualPublication = (ActualPublication) publication;
+            PublishArtifactInfo artifactInfo = new PublishArtifactInfo(getPublicationArtifactId(actualPublication), "module", "module", null, moduleMetadata);
+            addArtifactToDeployDetails(actualPublication, builder, artifactInfo);
+        }
+    }
+}
